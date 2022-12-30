7c74cc2e06518c05333b21d6e81864d6086b007c
RAJU SOMVANSHI <RAJU SOMVANSHI@DESKTOP-S51C6EU>
2022-12-30 15:46:39 +0530

    commit 3

---
+++ AssigmentSwara/src/main/java/Assignment/DivisionException.java
@@ -0,0 +1,27 @@
+package Assignment;
+import java.util.Scanner;
+
+
+	public class DivisionException {
+		public static void main(String[] args) 
+		{
+		   Scanner sc=new Scanner(System.in);
+		   System.out.println("Enter the value");
+		   int i=sc.nextInt();
+		   int j=sc.nextInt();
+		   int a;
+		   try
+		   {
+			    a=i/j;
+			   System.out.println("The Division Of two Number:"+a);en
+		   }
+		   catch(Exception e)
+		   {
+			   System.out.println("The Exception is :"+e);
+		   }
+		}
+
+	}
+
+
+
